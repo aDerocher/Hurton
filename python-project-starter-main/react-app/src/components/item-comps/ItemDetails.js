49420a9e58aa19e580ff99ff8ac2243a75d864cd
Andrew Derocher <am.derocher@gmail.com>
2021-10-27 18:03:51 -0700

    interesting code sc

--- python-project-starter-main/react-app/src/components/item-comps/ItemDetails.js
+++ python-project-starter-main/react-app/src/components/item-comps/ItemDetails.js
@@ -95,7 +95,6 @@ const ItemDetails = () => {
             cartStorage.setItem('cart', JSON.stringify({}));
             cart = localStorage.getItem('cart');
         }
-        console.log(JSON.parse(cart))
         cart = JSON.parse(cart)
         if (cart[`${data.item_id}_${data.item_color}_${data.item_size}`]){
             cart[`${data.item_id}_${data.item_color}_${data.item_size}`].quantity += data.quantity
